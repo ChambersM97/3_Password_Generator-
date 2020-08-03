4233de0db63f67cee2393ff270f3ffc6a4acaf8e
Chambers Mansour <chambersmansour97@gmail.com>
2020-08-03 01:16:00 -0500

    discovered .join command

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,4 +1,5 @@
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+console.log(upperCase.join(""));
 
 // Assignment Code
 // Looks for id of generate and selects it
@@ -12,7 +13,6 @@ generateBtn.addEventListener("click", writePassword);
 
 // THIS IS WHEN THE BUTTON IS CLICKED.
 
-//allows satrts making password
 // Write password to the #password input
 
 function writePassword() {
@@ -22,21 +22,33 @@ function writePassword() {
   passwordText.value = password;
 
   function generatePassword() {
-    var useUpperCase = confirm("Would you like to use upper case letters in your password?");
-    var useLowerCase = confirm("Would you like to use lower case letters in your password?");
-    var useNumbers = confirm("Would you like to use numbers in your password?");
-    var useSpecial = confirm("Would you like to use special characters in your password?");
-    var characterLength = askCharacterLength;
+    var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var specialCharacters = ["!","@","#","$","%","^","&","*","(",")"]; 
+
+    
+      var useUpperCase = confirm("Would you like to use upper case letters in your password?");
+      var useLowerCase = confirm("Would you like to use lower case letters in your password?");
+      var useNumbers = confirm("Would you like to use numbers in your password?");
+      var useSpecial = confirm("Would you like to use special characters in your password?");
+    //var characterLength = askCharacterLength;
   
-    //for (var i = 0; i < characterLength; i++)
+    for (var i = 0; i < characterLength; i++) {
+
+      //var word
+
+    }
     if (useUpperCase === true || useLowerCase === true || useNumbers === true || useSpecial === true) {
       var askCharacterLength = prompt("Between 8-128, Choose how many characters you want in your password");
+
     } else {
       alert("You must pick at least one category! Try again.");
       generatePassword();
     }
-      if (useUpperCase === true && useLowerCase === true && useNumbers === true && useSpecial === true) {
-        if (characterLength > 7 && characterLength < 129) {
+
+    if (useUpperCase === true && useLowerCase === true && useNumbers === true && useSpecial === true) {
+        if (askCharacterLength > 7 && askCharacterLength < 129) {
         
       } else {
         alert("You must choose between 8-128 characters. Try again!");
@@ -44,35 +56,34 @@ function writePassword() {
   
   } else if (useUpperCase === true && useLowerCase !== true && useNumbers !== true && useSpecial !== true) {
         if (askCharacterLength > 7 && askCharacterLength < 129) {
-          var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-          alert("hi");
+
       } else {
         alert("You must choose between 8-128 characters. Try again!");
       }
   
   } else if (useUpperCase === true && useLowerCase === true && useNumbers !== true && useSpecial !== true) {
-        if (characterLength > 7 && characterLength < 129) {
+        if (askCharacterLength > 7 && characterLength < 129) {
   
       } else {
         alert("You must choose between 8-128 characters. Try again!");
       }
   
   } else if (useUpperCase === true && useLowerCase === true && useNumbers === true && useSpecial !== true) {
-        if (characterLength > 7 && characterLength < 129) {
+        if (askCharacterLength > 7 && askCharacterLength < 129) {
   
       } else {
         alert("You must choose between 8-128 characters. Try again!");
       }
   
   } else if (useUpperCase !== true && useLowerCase === true && useNumbers !== true && useSpecial !== true) {
-        if (characterLength > 7 && characterLength < 129) {
+        if (askCharacterLength > 7 && askCharacterLength < 129) {
   
       } else {
         alert("You must choose between 8-128 characters. Try again!");
       }
   
   } else if (useUpperCase !== true && useLowerCase === true && useNumbers === true && useSpecial !== true) {
-        if (characterLength > 7 && characterLength < 129) {
+        if (askCharacterLength > 7 && askCharacterLength < 129) {
   
       } else {
           alert("You must choose between 8-128 characters. Try again!");
@@ -80,28 +91,28 @@ function writePassword() {
   
   } else if (useUpperCase !== true && useLowerCase === true && useNumbers === true && useSpecial === true) {
   
-        if (characterLength > 7 && characterLength < 129) {
+        if (askCharacterLength > 7 && askCharacterLength < 129) {
   
       } else {
           alert("You must choose between 8-128 characters. Try again!");
         }
   
   } else if (useUpperCase !== true && useLowerCase !== true && useNumbers === true && useSpecial !== true) {
-        if (characterLength > 7 && characterLength < 129) {
+        if (askCharacterLength > 7 && askCharacterLength < 129) {
   
       } else {
           alert("You must choose between 8-128 characters. Try again!");
         }
   
   } else if (useUpperCase !== true && useLowerCase !== true && useNumbers === true && useSpecial === true) {
-        if (characterLength > 7 && characterLength < 129) {
+        if (askCharacterLength > 7 && askCharacterLength < 129) {
   
       } else {
           alert("You must choose between 8-128 characters. Try again!");
         }
   
   } else if (useUpperCase !== true && useLowerCase !== true && useNumbers !== true && useSpecial === true){;
-    }   if (characterLength > 7 && characterLength < 129) {
+    }   if (askCharacterLength > 7 && askCharacterLength < 129) {
   
       }
   
@@ -109,8 +120,4 @@ function writePassword() {
   
 }
 
-//var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-//var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-//var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-//var specialCharacters = ["!","@","#","$","%","^","&","*","(",")"]; 
 
