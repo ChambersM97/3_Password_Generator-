4ae6cb1bba0254c839fa2398d46adf405c377382
Chambers Mansour <chambersmansour97@gmail.com>
2020-08-03 01:36:26 -0500

    found way to make string

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,5 +1,9 @@
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-console.log(upperCase.join(""));
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var x = upperCase.concat(lowerCase);
+console.log(x.join(""));
+
+//console.log(upperCase.join());
 
 // Assignment Code
 // Looks for id of generate and selects it
