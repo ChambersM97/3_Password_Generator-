ae53f5d49b9a740ba91c7b675697802b8d4e1174
Chambers Mansour <chambersmansour97@gmail.com>
2020-08-06 11:57:10 -0500

    deleted generatepassword redoing code

--- 02-Homework/notes.js
+++ 02-Homework/notes.js
@@ -1,29 +1,34 @@
 function generatePassword() {
     for (var i = 0; i < charLength; i++) {
         if (upCase === true) {
-            writeUpperCase();
-            charLength - 1;
+
         }
         else if (upCase === false) {
         };
         if (lowCase === true) {
-            writeLowerCase();
-            charLength - 1;
+
         }
         else if (lowCase === false) {
         };
         if (numb === true) {
-            writeRanNumber();
-            charLength - 1;
+
         }
         else if (numb === false) {
         };
         if (sym === true) {
-            writeRanSymbol();
-            charLength - 1;
+
         }
         else if (sym === false) {
         };  
     }
 
+}
 
+writeUpperCase();
+charLength - 1;
+writeLowerCase();
+charLength - 1;
+writeRanNumber();
+charLength - 1;
+writeRanSymbol();
+charLength - 1;
