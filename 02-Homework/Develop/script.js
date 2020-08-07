86c41a261c55b268c8b1403a06c5eea77f122273
Chambers Mansour <chambersmansour97@gmail.com>
2020-08-07 01:41:41 -0500

    new logic works about to finish rest of conditionals to save into final version.

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -23,17 +23,29 @@ function writePassword() {
 
 // we establish the arrays for all the characters we need, seperating them by id
 //conditionals
+var lowerUpperNumbersSymbols = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+"!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerUpperSymbols = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+"!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+var lowerUpperNumbers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", 
+"V", "W", "X", "Y", "Z","0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var lowerNumbersSymbols = []
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 var upperLowerCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 // The confirms are used to determ if the generatePassword() function will include that specificed character set
-var useUpperCase = confirm("Would you like to use upper case letters in your password?");
-var useLowerCase = confirm("Would you like to use lower case letters in your password?");
+var useLowerCase = confirm("Would you like to use upper case letters in your password?");
+var useUpperCase = confirm("Would you like to use lower case letters in your password?");
 var useNumbers = confirm("Would you like to use numbers in your password?");
 var useSymbols = confirm("Would you like to use special characters in your password?");
 var askCharacterLength = parseInt(prompt("Well... How many characters would you like to have in your password! (must be between 8 and 128)"));
@@ -49,11 +61,20 @@ function generatePassword() {
 
   for (var i = 0; i < askCharacterLength; i++) {
 
-    if (useLowerCase === true && useUpperCase === true ){
+    if (useLowerCase === true && useUpperCase === true && useNumbers === true && useSymbols == true) {
 
-      endPassword += ((upperLowerCase[(Math.floor(Math.random() * upperLowerCase.length))]));
+      endPassword += ((lowerUpperNumbersSymbols[(Math.floor(Math.random() * lowerUpperNumbersSymbols.length))]));
+    }
 
-    } else if (useLowerCase === true) {
+    else if (useLowerCase === true && useUpperCase === true && useSymbols === true) {
+      endPassword += ((lowerUpperSymbols[(Math.floor(Math.random() * lowerUpperSymbols.length))]));
+    }
+
+    else if (useLowerCase === true && useUpperCase === true && useNumbers === true) {
+
+      endPassword += ((lowerUpperNumbers[(Math.floor(Math.random() * lowerUpperNumbers.length))]));
+
+    } else if (useLowerCase === true && useNumbers === true && useSymbols === true) {
 
       endPassword += ((lowerCase[(Math.floor(Math.random() * lowerCase.length))]));
 
